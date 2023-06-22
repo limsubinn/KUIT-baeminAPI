1c13342d1e0ceea634b27148acef2b4a5b33f85f
limsubinn <mymyx02@naver.com>
2023-06-22 13:46:49 +0900

    chore: @SpringBootTest 주석 처리

--- src/test/java/com/example/baemin/BaeminApplicationTests.java
+++ src/test/java/com/example/baemin/BaeminApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.baemin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class BaeminApplicationTests {
 
 	@Test
